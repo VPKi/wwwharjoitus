702f672a53101082d7a477fd884a1e60eb4be861
Tolaaks <toni.laaksonen93@gmail.com>
2016-04-20 22:39:25 +0300

    Update index.js

--- esimerkki1/routes/index.js
+++ esimerkki1/routes/index.js
@@ -1,50 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
+//Subdomainien  määrittely, kertoo ohjelmalle mitä kussakin subdomainissa renderoidään
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Pelipankki R5' });
 });
 
-/*Get hello world page */
+/* GET Hello World page. */
 router.get('/helloworld', function(req, res) {
-	
-res.render('helloworld', {title: "hello, world!"});
+    res.render('helloworld', { title: 'Hello, World!' });
 });
 
-/*get user list page */
-router.get("/userlist", function(req,res) {
-	var db = req.db;
-	var collection = db.get("usercollection");
-	collection.find({},{},function(e,docs) {
-		res.render("userlist", {
-			"userlist" : docs
-		});
-	});
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+    var db = req.db;
+    var collection = db.get('usercollection');
+    collection.find({},{},function(e,docs){
+        res.render('userlist', {
+            "userlist" : docs
+        });
+    });
 });
 
-/*get new user page */
-router.get("/newuser", function(req,res) {
-	res.render("newuser", {title: "add new user" });
+/* GET New User page. This adds users and asks their info.*/
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Lisää Uusi Peli' });
 });
 
-/* POST to Add User Service */
+/* POST to Add User Service. THis is not a subdomain! This is a process.*/
 router.post('/adduser', function(req, res) {
 
     // Set our internal DB variable
     var db = req.db;
 
     // Get our form values. These rely on the "name" attributes
-    var userName = req.body.username;
-    var userEmail = req.body.useremail;
+    var nimi = req.body.nimi;
+    var alusta = req.body.alusta;
+	var vuosi = req.body.vuosi;
+	var kehittaja = req.body.kehittaja;
+	var julkaisija = req.body.julkaisija;
+	var genre = req.body.genre;
 
     // Set our collection
     var collection = db.get('usercollection');
 
     // Submit to the DB
     collection.insert({
-        "username" : userName,
-        "email" : userEmail
+        "nimi" : nimi,
+	"vuosi" : vuosi,
+	"alusta" : alusta,
+        "kehittäjä" : kehittaja,
+	"julkaisija" : julkaisija,
+	"genre" : genre
     }, function (err, doc) {
         if (err) {
             // If it failed, return error
